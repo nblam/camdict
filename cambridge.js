a1c375fef532ec2130623e0eade23e6c7a2e1870
Lam Nguyen <rimikio122@gmail.com>
2023-06-10 23:46:24 +0700

    remove chinese search

--- cambridge.js
+++ cambridge.js
@@ -10,7 +10,7 @@ class encn_Cambridge {
     let locale = await api.locale();
     if (locale.indexOf("CN") != -1) return "剑桥英汉双解(简体)";
     if (locale.indexOf("TW") != -1) return "劍橋英漢雙解(簡體)";
-    return "Cambridge EN->CN Dictionary (hello)";
+    return "cambridge";
   }
 
   setOptions(options) {
@@ -34,8 +34,7 @@ class encn_Cambridge {
       else return node.innerText.trim();
     }
 
-    let base =
-      "https://dictionary.cambridge.org/search/english-chinese-simplified/direct/?q=";
+    let base = "https://dictionary.cambridge.org/search/english/direct/?q=";
     let url = base + encodeURIComponent(word);
     let doc = "";
     try {
